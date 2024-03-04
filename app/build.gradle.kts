e2825f5aa8f9cc45f16a7e35ca55140cefd43d7f
David <david.noctis99@gmail.com>
2024-03-04 10:17:45 +0100

    App terminada.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -38,11 +38,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -298,7 +298,7 @@ private fun DessertsSoldInfo(dessertsSold: Int, modifier: Modifier = Modifier) {
 fun MyDessertClickerAppPreview() {
     DessertClickerTheme {
         DessertClickerApp(
-            uiState = DessertUiState(),
+            uiState = DessertUIState(),
             onDessertClicked = {}
         )
     }
